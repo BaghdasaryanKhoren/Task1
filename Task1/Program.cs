0fdb2dbee5a3cf0814a57d3b4d5f843f0ec63342
khoren.baghdasaryan96@outlook.com <khoren.baghdasaryan96@outlook.com>
2019-06-20 00:38:05 +0400

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static int func(int number)
+        {
+            int num = 0;
+            if (number <= 9)
+                return number;
+            
+            if(number > 9)
+            {
+                int qanordyMnacordov = number % 10;
+                number = number / 10;
+                num = number + qanordyMnacordov;
+                
+            }
+            return func(num);
+        }
+        static void Main(string[] args)
+        {
+            for (;;)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                int result = func(number);
+                Console.WriteLine(result);
+            }
+            
+        }
+    }
+}
